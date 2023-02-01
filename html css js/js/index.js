73af7d405384254b37fd9e474811b4ec9db7d391
0112shpark <000112shpark@naver.com>
2023-02-01 16:35:16 +0900

    javascript intro

--- html css js/js/index.js
+++ html css js/js/index.js
@@ -1,5 +1,30 @@
-const els = document.querySelectorAll('div')
-els.forEach(el =>{
-  console.log(el.dataset.fruit)
-})
-console.log("안녕")
+console.log("안녕");
+let user = {
+  name: "dd",
+  age: 44,
+  isvalud: true,
+};
+function hello() {
+  return 13;
+}
+console.log(hello());
+let boxEl = document.querySelector(".box1");
+console.log(boxEl);
+
+// boxEl.addEventListener("click", function () {
+//   console.log("Click!");
+//   boxEl.classList.add("active");
+//   console.log(boxEl.classList.contains("active"));
+//   boxEl.classList.remove("active");
+// });
+let boxEls = document.querySelectorAll(".box");
+boxEls.forEach(function (name, index) {
+  name.classList.add(`order-${index + 1}`);
+  console.log(index, name);
+});
+let boxElss = document.querySelector(".box");
+console.log(boxElss.textContent);
+boxElss.addEventListener("click", function () {
+  boxElss.textContent = "box!";
+  console.log(boxElss.textContent);
+});
